b9dfa906e8246bbbac2d83148dd6c474779cf53f
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2022-02-24 18:18:44 +0100

    Update settings fragment

--- app/src/main/java/dev/patri9ck/a2ln/main/ui/settings/SettingsFragment.java
+++ app/src/main/java/dev/patri9ck/a2ln/main/ui/settings/SettingsFragment.java
@@ -1,14 +1,14 @@
 package dev.patri9ck.a2ln.main.ui.settings;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import dev.patri9ck.a2ln.databinding.FragmentSettingsBinding;
 
@@ -18,15 +18,16 @@ public class SettingsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        SettingsViewModel settingsViewModel =
-                new ViewModelProvider(this).get(SettingsViewModel.class);
-
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
-        final TextView textView = binding.textSettings;
-        settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        binding.permissionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS));
+            }
+        });
+
+        return binding.getRoot();
     }
 
     @Override
