8d98e68553487f4bf54925d16c8408695e77fe78
Dominik <36272047+Domi04151309@users.noreply.github.com>
2022-03-13 15:06:21 +0100

    Enhance UI (#12)
    * Add different launch activity
    
    * Add apps fragment
    
    * Add devices fragment
    
    * Update settings fragment
    
    * Clean up
    
    * Use floating action button for adding
    
    * Typo
    
    * General improvements
    
    Co-authored-by: patri9ck <patri9ck@gmail.com>

---
+++ app/src/main/java/dev/patri9ck/a2ln/main/ui/SettingsFragment.java
@@ -0,0 +1,34 @@
+package dev.patri9ck.a2ln.main.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import dev.patri9ck.a2ln.databinding.FragmentSettingsBinding;
+
+public class SettingsFragment extends Fragment {
+
+    private FragmentSettingsBinding binding;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentSettingsBinding.inflate(inflater, container, false);
+
+        binding.permissionsButton.setOnClickListener(view -> startActivity(new Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS)));
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        binding = null;
+    }
+}
