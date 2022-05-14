7fabcf3b8bb635f349c0e63d89cc59359d7e70fd
Patrick <patri9ck@gmail.com>
2022-05-14 16:44:27 +0200

    Added help button (#14)

--- app/src/main/java/dev/patri9ck/a2ln/main/ui/SettingsFragment.java
+++ app/src/main/java/dev/patri9ck/a2ln/main/ui/SettingsFragment.java
@@ -1,6 +1,7 @@
 package dev.patri9ck.a2ln.main.ui;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.LayoutInflater;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import dev.patri9ck.a2ln.R;
 import dev.patri9ck.a2ln.databinding.FragmentSettingsBinding;
 
 public class SettingsFragment extends Fragment {
@@ -18,6 +20,7 @@ public class SettingsFragment extends Fragment {
         FragmentSettingsBinding binding = FragmentSettingsBinding.inflate(inflater, container, false);
 
         binding.permissionsButton.setOnClickListener(view -> startActivity(new Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS)));
+        binding.helpButton.setOnClickListener(view -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.help_url)))));
 
         return binding.getRoot();
     }
