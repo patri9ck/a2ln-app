c4fc2c66c8ec79c0d9c9a3d1a5f4d8fa569becf8
patri9ck <patri9ck@gmail.com>
2022-09-28 21:42:10 +0200

    Formatting, switched back to Java 8

--- app/src/main/java/dev/patri9ck/a2ln/log/KeptLog.java
+++ app/src/main/java/dev/patri9ck/a2ln/log/KeptLog.java
@@ -14,7 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 package dev.patri9ck.a2ln.log;
 
 import android.util.Log;
