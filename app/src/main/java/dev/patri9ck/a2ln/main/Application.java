c5f7c1a369f3bd0f355d9f8a9d413f60dc208b7d
Niko Diamadis <niko@cyb3rko.de>
2024-08-08 18:08:49 +0200

    Material 3 adoption (#46)
    * Change colors and themes to Material 3
    
    * Simplify dialogs
    
    * Adapt spacing and view tool texting
    
    * Update Gradle and dependencies
    
    * Use consistent input type for IPs
    
    * Fixed some spacings, renaming and added scrolling for pair dialog (for landscape mode)
    
    * Enable Material You colors
    
    * Fix navigation bar color
    
    ---------
    
    Co-authored-by: patri9ck <patri9ck@gmail.com>

--- app/src/main/java/dev/patri9ck/a2ln/main/Application.java
+++ app/src/main/java/dev/patri9ck/a2ln/main/Application.java
@@ -1,6 +1,6 @@
 /*
  * Android 2 Linux Notifications - A way to display Android phone notifications on Linux
- * Copyright (C) 2023  patri9ck and contributors
+ * Copyright (C) 2024  patri9ck and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,25 +15,14 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-package dev.patri9ck.a2ln.log;
+package dev.patri9ck.a2ln.main;
 
-import android.view.LayoutInflater;
+import com.google.android.material.color.DynamicColors;
 
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
-
-import dev.patri9ck.a2ln.R;
-
-public class LogDialogBuilder extends MaterialAlertDialogBuilder {
-
-    public LogDialogBuilder(String log, LayoutInflater layoutInflater) {
-        super(layoutInflater.getContext(), R.style.Dialog);
-
-        setTitle(R.string.log_dialog_title);
-        setMessage(log);
-        setNegativeButton(R.string.cancel, null);
-    }
-
-    public LogDialogBuilder(KeptLog keptLog, LayoutInflater layoutInflater) {
-        this(keptLog.format(), layoutInflater);
+public class Application extends android.app.Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        DynamicColors.applyToActivitiesIfAvailable(this);
     }
 }
