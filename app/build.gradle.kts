5983bfe33d309ba6ee1325ddff44f59ddd37facb
Patrick Zwick <git@patri9ck.dev>
2026-06-13 01:12:53 +0200

    Update to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         minSdk = 27
         targetSdk = 31
 
-        versionCode = 5
-        versionName = "1.1.1"
+        versionCode = 6
+        versionName = "1.2.0"
     }
 
     compileOptions {
